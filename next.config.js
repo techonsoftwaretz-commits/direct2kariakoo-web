bf37a83781c7260fc1393faf85e39849a994e717
techonsoftwaretz-commits <techonsoftwaretz@gmail.com>
2025-10-25 05:42:12 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,23 @@
-/** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig = {
   images: {
     unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "direct2kariakoo.co.tz", pathname: "/**" },
       { protocol: "http", hostname: "127.0.0.1", port: "8000", pathname: "/**" },
-      { protocol: "http", hostname: "localhost", port: "8000", pathname: "/**" }
-    ]
+      { protocol: "http", hostname: "localhost", port: "8000", pathname: "/**" },
+    ],
   },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   trailingSlash: true,
+  output: "export",
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
-  }
+  },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
