b664fb7953f989c60cdc05bf4456fabbfa6ed066
techonsoftwaretz-commits <techonsoftwaretz@gmail.com>
2025-10-28 00:45:32 +0300

    Updated next.config.js and tsconfig.json

--- next.config.js
+++ next.config.js
@@ -1,23 +1,24 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
-const nextConfig = {
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      { protocol: "https", hostname: "direct2kariakoo.co.tz", pathname: "/**" },
-      { protocol: "http", hostname: "127.0.0.1", port: "8000", pathname: "/**" },
-      { protocol: "http", hostname: "localhost", port: "8000", pathname: "/**" },
-    ],
-  },
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  trailingSlash: true,
-  output: "export",
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
-};
-
-module.exports = withBundleAnalyzer(nextConfig);
+    enabled: process.env.ANALYZE === "true",
+  });
+  
+  const nextConfig = {
+    images: {
+      unoptimized: true,
+      remotePatterns: [
+        { protocol: "https", hostname: "direct2kariakoo.co.tz", pathname: "/**" },
+        { protocol: "http", hostname: "127.0.0.1", port: "8000", pathname: "/**" },
+        { protocol: "http", hostname: "localhost", port: "8000", pathname: "/**" },
+      ],
+    },
+    eslint: { ignoreDuringBuilds: true },
+    typescript: { ignoreBuildErrors: true },
+    trailingSlash: true,
+    output: "export",
+    compiler: {
+      removeConsole: process.env.NODE_ENV === "production",
+    },
+  };
+  
+  module.exports = withBundleAnalyzer(nextConfig);
+  
